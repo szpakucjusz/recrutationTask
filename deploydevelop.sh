74b476c84ac7de528a23a583988e04c0ba932275
szpakucjusz <lukaszszpakowski2@gmail.com>
2020-01-19 09:23:36 +0100

    chore: base app congiguration

--- deploydevelop.sh
+++ deploydevelop.sh
@@ -1,4 +1,4 @@
 git pull origin master
-docker run --rm -v $(pwd):/app composer install --optimize-autoloader --no-dev
+docker run --rm -v $(pwd):/app composer install
 docker exec -it app-recrutation php artisan migrate && docker exec -it app-recrutation php artisan config:cache && docker exec -it app-recrutation php artisan route:cache
 docker run --rm -v $(pwd):/app composer dump-autoload
